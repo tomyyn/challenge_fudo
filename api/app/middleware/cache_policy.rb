acf1fc5abe32506b96acda9ede59a7864b20ea96
tomyyn <tomasnaiouf@gmail.com>
2025-01-23 12:05:52 -0300

    Añadir manejo de archivos estáticos

---
+++ api/app/middleware/cache_policy.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rack'
+
+# Middleware para personalizar el header Cache-Control de determinadas respuestas.
+class CachePolicy
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+
+    case env['PATH_INFO']
+    when '/AUTHORS'
+      headers['cache-control'] = 'public, max-age=86400'
+    when '/openapi.yaml'
+      headers['cache-control'] = 'no-store'
+    end
+
+    [status, headers, body]
+  end
+end
--- api/config.ru
+++ api/config.ru
@@ -2,7 +2,11 @@
 
 require 'rack'
 require './app/application'
+require './app/middleware/cache_policy'
 
 use(Rack::Reloader, 0) if ENV['RACK_ENV'] == 'development'
+
+use CacheControl
+use Rack::Static, urls: ['/'], root: 'public', cascade: true
 
 run Application.new
