3a60d7aa600c85c5e5fbac53b25831bd0d6376c8
tomyyn <tomasnaiouf@gmail.com>
2025-01-26 10:55:12 -0300

    Mejorar tests de webhook

--- api/spec/unit/async_tasks/create_product_task_spec.rb
+++ api/spec/unit/async_tasks/create_product_task_spec.rb
@@ -2,6 +2,7 @@
 
 require 'spec_helper'
 require './app/async_tasks/create_product_task'
+require './app/models/product'
 
 RSpec.describe CreateProductTask do
   describe '#perform' do
@@ -16,6 +17,16 @@ RSpec.describe CreateProductTask do
       expect(RequestService).not_to receive(:new)
       task = CreateProductTask.new
       task.perform(name: 'Product', external_id: '1234', log_id: 1)
+    end
+
+    context 'when webhook_url is provided' do
+      it 'sends the webhook' do
+        allow(Product).to receive(:create).and_return(true)
+        allow(Product).to receive(:find_creation_log).with(1).and_return({ log_id: 1, status: 'created' })
+        expect_any_instance_of(RequestService).to receive(:execute)
+        task = CreateProductTask.new
+        task.perform(name: 'Product', external_id: '1234', log_id: 1, webhook_url: 'http://example.com')
+      end
     end
   end
 end
